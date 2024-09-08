7d9fcf4bd8efe38d76d3aff88a6fd1de4430b352
Andreas Giereck Kristiansen <giereck@hotmail.com>
2024-09-08 23:18:41 +0200

    Use clap

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-#[allow(unused_imports)]
+use anyhow::Ok;
+use clap::{Parser, Subcommand};
 use std::env;
-#[allow(unused_imports)]
+
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[derive(Parser)]
+#[clap(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    commands: Commands,
+}
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.commands {
+        Commands::Init => {
+            let cwd = env::current_dir()?;
+            println!("Initializing git in {:#?}", cwd);
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory");
+            Ok(())
+        }
+    }
 }
